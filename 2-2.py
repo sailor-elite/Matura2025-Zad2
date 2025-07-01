1ce22eaf9acdf127b9ec55d94c9068ed3faa8989
Mysiorek123 <mysior1239@gmail.com>
2025-07-01 20:53:51 +0200

    Solved exercise 2.2

--- 2-2.py
+++ 2-2.py
@@ -1,21 +1,27 @@
-plik = open ("symbole_przyklad.txt", "r")
-
+plik = open("symbole.txt", "r")
 
 tablica = []
+wyniki = []
+ilosc_kwadratow = 0
 
 for linia in plik :
     linia = linia.strip()
     tablica.append(linia)
-    #print("palindrom: ")
-    #print (linia)
 
-print(tablica)
 
-for i in range (1, len(tablica)-1):
-    for j in range (1, 11):
+for i in range (1,len(tablica) -1):
+    for j in range (1,11):
         s = tablica[i][j]
-        print(s, "tablica nr: ", i, "znak nr: ", j)
+        #print (s, "tablica nr: ", i, "znak nr:", j)
+        if s == tablica[i][j-1] and s == tablica[i][j+1]:
+            if s == tablica[i+1][j] and s == tablica[i-1][j]:
+                if tablica[i+1][j] == tablica[i+1][j-1] and tablica[i+1][j] == tablica[i+1][j+1]:
+                    if tablica[i-1][j] == tablica[i-1][j-1] and tablica[i-1][j] == tablica[i-1][j+1] and tablica[i-1][j]:
+                        ilosc_kwadratow = ilosc_kwadratow+1
+                        wyniki.append(i+1)
+                        wyniki.append(j+1)
 
-# 1. Dokończ zadanie sprawdzając warunki tabeli (macierzy)
-# 2. Przypisz zadanie do tablicy o nazwie wyniki. Wykorzystaj funkcję .append(znak)
-# 3. Wypisz za pomocą pętli i print wyniki. Wyniki powinny być zapisane jako wiesz, kolumna
+
+print(ilosc_kwadratow,wyniki[0:ilosc_kwadratow*2:1])
+
+plik.close()
