1f9d32c4abf9dc6cd0d88caf5ba6de1c6527086b
Jakub <jakub.j.bednarczyk@gmail.com>
2025-07-02 19:43:53 +0200

    4.1 zadanie

--- Matura 2024/4-1.py
+++ Matura 2024/4-1.py
@@ -3,10 +3,8 @@ plik = open("liczby_przyklad.txt", "r")
 tablica = []
 tablica_liczby  = []
 tablica_dzielniki = []
-wynik_dzielniki = 0
-j = 0
-i = 0
-
+licznik = 0
+liczba = 0
 
 for linia in plik:
     linia = linia.strip()
@@ -14,16 +12,18 @@ for linia in plik:
     tablica.append(linia)
 for dzielnik in range (0,len(tablica[0])):
     tablica_dzielniki.append(tablica[0][dzielnik])
-
 for liczba in range(0, len(tablica[1])):
     tablica_liczby.append(tablica[1][liczba])
 
-    while i != len(tablica_liczby):
-        while j < len(tablica_dzielniki):
-            s = tablica_liczby[i]
-            j = j +1
-            print (s)
-        i = i +1
+
+for dzielnik in tablica_dzielniki:
+    for liczba in tablica_liczby:
+        if int(liczba) % int(dzielnik) == 0:
+            licznik += 1
+            break
+
+
+
 
 
 
